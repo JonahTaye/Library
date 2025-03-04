e92b31696ca64e5589d6d721873fb5b20e1723ad
jonah <jonah.softwaredev@gmail.com>
2025-03-04 11:35:37 +0300

    feat: add event listeners and createCard() function

--- libraryScript.js
+++ libraryScript.js
@@ -18,5 +18,52 @@ Book.prototype.addBook = function() {
     myLibrary.push(bookInfo)
 }
 
+const form = document.querySelector("form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    let newBook = new Book(
+        document.getElementById("title").value, 
+        document.getElementById("author").value, 
+        document.getElementById("pages").value, 
+        document.getElementById("book-status").value
+    )
 
-console.log(myLibrary)
+    newBook.addBook()
+    createCard()
+    form.reset()
+    console.log(myLibrary)
+})
+
+function createCard() {
+    let card = document.createElement("div")
+    let btn = document.createElement("button")
+    card.textContent = "Book"
+    btn.textContent = "Remove"
+    btn.classList.add("remove")
+    card.classList.add("cards")
+    ids = Math.floor(Math.random() * 10)
+    card.id = "id" + ids
+    btn.id = "id" + ids
+    card.style.border = "1px solid black"
+
+    let newBook = myLibrary.at(-1)
+    newBook.map(bookInfo => {
+        let info = document.createElement("div")
+        info.textContent = bookInfo
+        card.appendChild(info)
+        card.remov
+    })
+    card.appendChild(btn)
+    document.body.appendChild(card)
+}
+
+
+document.addEventListener("click", event => {
+    if (event.target.classList.contains("remove")) {
+        let card = document.querySelector(`.cards#${event.target.id}`)
+        document.body.removeChild(card)
+        myLibrary.map
+    }
+})
+
